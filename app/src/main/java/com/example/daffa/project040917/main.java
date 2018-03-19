0eb79bc529fcf365a99603bb5e82bc398982c49f
Abiyyu Daffa' Alam <daffaalam@gmail.com>
2018-03-19 21:34:24 +0700

    update 19.03.18

--- app/src/main/java/com/example/daffa/project040917/main.java
+++ app/src/main/java/com/example/daffa/project040917/main.java
@@ -1,5 +1,6 @@
 package com.example.daffa.project040917;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,12 +23,12 @@ public class main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //2 - menghubungkan id xml ke java
-        rbxrpl = (RadioButton)findViewById(R.id.xrpl);
-        rbxtkj = (RadioButton)findViewById(R.id.xtkj);
-        rbxirpl = (RadioButton)findViewById(R.id.xirpl);
-        rbxitkj = (RadioButton)findViewById(R.id.xitkj);
-        rbxiirpl = (RadioButton)findViewById(R.id.xiirpl);
-        rbxiitkj = (RadioButton)findViewById(R.id.xiitkj);
+        rbxrpl = (RadioButton) findViewById(R.id.xrpl);
+        rbxtkj = (RadioButton) findViewById(R.id.xtkj);
+        rbxirpl = (RadioButton) findViewById(R.id.xirpl);
+        rbxitkj = (RadioButton) findViewById(R.id.xitkj);
+        rbxiirpl = (RadioButton) findViewById(R.id.xiirpl);
+        rbxiitkj = (RadioButton) findViewById(R.id.xiitkj);
         btvld = (Button) findViewById(R.id.bvld);
         btvld.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,33 +55,28 @@ public class main extends AppCompatActivity {
     }
 
     //   - membuat methode validasi radiobutton
-    public void vldsrb(){
+    @SuppressLint("SetTextI18n")
+    public void vldsrb() {
 
-        if(rbxrpl.isChecked()) {
+        if (rbxrpl.isChecked()) {
             Toast.makeText(main.this, "Anda memilih Kelas 10 jurusan RPL", Toast.LENGTH_SHORT).show();
             txhsl.setText("Kelas 10 Jurusan RPL");
-        }
-        else if(rbxtkj.isChecked()){
+        } else if (rbxtkj.isChecked()) {
             Toast.makeText(main.this, "Anda memilih Kelas 10 jurusan TKJ", Toast.LENGTH_SHORT).show();
             txhsl.setText("Kelas 10 Jurusan TKJ");
-        }
-        else if(rbxirpl.isChecked()){
+        } else if (rbxirpl.isChecked()) {
             Toast.makeText(main.this, "Anda memilih Kelas 11 jurusan RPL", Toast.LENGTH_SHORT).show();
             txhsl.setText("Kelas 11 Jurusan RPL");
-        }
-        else if(rbxitkj.isChecked()){
+        } else if (rbxitkj.isChecked()) {
             Toast.makeText(main.this, "Anda memilih Kelas 11 jurusan TKJ", Toast.LENGTH_SHORT).show();
             txhsl.setText("Kelas 11 Jurusan TKJ");
-        }
-        else if(rbxiirpl.isChecked()){
+        } else if (rbxiirpl.isChecked()) {
             Toast.makeText(main.this, "Anda memilih Kelas 12 jurusan RPL", Toast.LENGTH_SHORT).show();
             txhsl.setText("Kelas 12 Jurusan RPL");
-        }
-        else if(rbxiitkj.isChecked()){
+        } else if (rbxiitkj.isChecked()) {
             Toast.makeText(main.this, "Anda memilih Kelas 12 jurusan TKJ", Toast.LENGTH_SHORT).show();
             txhsl.setText("Kelas 12 Jurusan TKJ");
-        }
-        else {
+        } else {
             Toast.makeText(main.this, "Pilihan tidak boleh kosong!!!", Toast.LENGTH_SHORT).show();
             txhsl.setText("ERROR!!!");
         }
@@ -88,9 +84,9 @@ public class main extends AppCompatActivity {
 
 
     //   - membuat methode uncheck all radiobutton
-    public void clrrb(){
+    public void clrrb() {
 
-        if (rbxrpl.isChecked() || rbxtkj.isChecked() || rbxirpl.isChecked() || rbxitkj.isChecked() || rbxiirpl.isChecked() || rbxiitkj.isChecked()){
+        if (rbxrpl.isChecked() || rbxtkj.isChecked() || rbxirpl.isChecked() || rbxitkj.isChecked() || rbxiirpl.isChecked() || rbxiitkj.isChecked()) {
             rbxrpl.setChecked(false);
             rbxtkj.setChecked(false);
             rbxirpl.setChecked(false);
@@ -102,13 +98,16 @@ public class main extends AppCompatActivity {
 
 
     //   - membuat methode kirim data ke activity data
-    public void snddata(){
+    public void snddata() {
 
-        Intent senddat = new Intent(getApplicationContext(), data.class);
-        senddat.putExtra("dthsl", txhsl.getText().toString());
-        startActivity(senddat);
-
+        if (!rbxtkj.isChecked() && !rbxrpl.isChecked() && !rbxitkj.isChecked() && !rbxirpl.isChecked() && !rbxiitkj.isChecked() && !rbxiirpl.isChecked()) {
+            Toast.makeText(this, "Pilihan Anda tidak Boleh Kosong...!!!", Toast.LENGTH_SHORT).show();
+        } else {
+            vldsrb();
+            Intent senddat = new Intent(getApplicationContext(), data.class);
+            senddat.putExtra("dthsl", txhsl.getText().toString());
+            startActivity(senddat);
+        }
     }
-
- }
+}
 
